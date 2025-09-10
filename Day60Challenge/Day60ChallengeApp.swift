6b6a27931f7d51e57ef567715af82d27ba163693
Santhosh2141 <snthshsrinivas@gmail.com>
2025-09-10 13:06:19 +0530

    Initial Commit

---
+++ Day60Challenge/Day60ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day60ChallengeApp.swift
+//  Day60Challenge
+//
+//  Created by Santhosh Srinivas on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Day60ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
